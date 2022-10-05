d7989a26a471061b566092c52a4e79262cff1f7a
sodiqoydinov <sodiq7898@gmail.com>
2022-10-05 15:51:15 +0500

    Last_updated

--- main.py
+++ main.py
@@ -1,34 +1,43 @@
 from flask import Flask, render_template, request, redirect
-import pickle
 
 app = Flask(__name__)
 
-
+global users_data
 users_data = []
 
 @app.route('/', methods=['GET','POST'])
 def about():
 	# if request.method=="POST":
 	# 	return request.form.get('my')
-	return render_template('main.html')
+	return render_template('testform.html')
 
 
 @app.route('/testform', methods=['POST','GET'])
 def testform():
-	ism=request.form.get('ism')
-	familiya = request.form.get('familiya')
-	ism2 = request.form.get('ism2')
+	fio=request.form.get('fio')
+	vaqt1 = request.form.get('vaqt1')
 	ambul_kart = request.form.get('ambul_kart')
+	uchastok = request.form.get('uchastok')
 	tel = request.form.get('tel')
-	vaqt = request.form.get('vaqt')
-	if not ism or not familiya or not ism2 or not ambul_kart or not tel or not vaqt:
-		return render_template('fail1.html',ism=ism, familiya=familiya,ism2=ism2,ambul_kart=ambul_kart,tel=tel,vaqt=vaqt)
-	return render_template('next1.html',ism=ism, familiya=familiya,ism2=ism2,ambul_kart=ambul_kart,tel=tel,vaqt=vaqt)
-	# users_data.append(f"{ism},{familiya},{ism2},{ambul_kart},{tel},{vaqt}")
+	vaqt2 = request.form.get('vaqt2')
+	if not fio or not vaqt1 or not ambul_kart or not tel or not vaqt2 or not uchastok:
+		return render_template('fail1.html',fio=fio, vaqt1=vaqt1,ambul_kart=ambul_kart,tel=tel,vaqt2=vaqt2,uchastok=uchastok)
+	return render_template('next1.html',fio=fio, vaqt1=vaqt1,ambul_kart=ambul_kart,tel=tel,vaqt2=vaqt2,uchastok=uchastok)
+	
+	users_data.append(f"{ism},{familiya},{ism2},{ambul_kart},{tel},{vaqt}")
 
-	
-	return render_template('testform.html')
-	
+@app.route('/fail1', methods=['POST','GET'])
+def failform():
+	fio=request.form.get('fio')
+	vaqt1 = request.form.get('vaqt1')
+	ambul_kart = request.form.get('ambul_kart')
+	uchastok = request.form.get('uchastok')
+	tel = request.form.get('tel')
+	vaqt2 = request.form.get('vaqt2')
+
+	return render_template('next1.html',fio=fio, vaqt1=vaqt1,ambul_kart=ambul_kart,tel=tel,vaqt2=vaqt2,uchastok=uchastok)
+
+
 @app.route('/test', methods=['GET','POST'])
 def test():
 
@@ -50,4 +59,10 @@ def result1():
 	s4 = request.form.get('inlineRadioOptions4')
 	s5 = request.form.get('inlineRadioOptions5')
 	s6 = request.form.get('inlineRadioOptions6')
-	return render_template('result_page.html',s1=s1,s2=s2,s3=s3,s4=s4,s5=s5,s6=s6)
+	s7 = request.form.get('inlineRadioOptions7')
+	s8 = request.form.get('inlineRadioOptions8')
+	s9 = request.form.get('inlineRadioOptions9')
+	s10 = request.form.get('inlineRadioOptions10')
+	s11 = request.form.get('inlineRadioOptions11')
+	s12 = request.form.get('inlineRadioOptions12')
+	return render_template('result_page.html',s1=s1,s2=s2,s3=s3,s4=s4,s5=s5,s6=s6,s7=s7,s8=s8,s9=s9,s10=s10,s11=s11,s12=s12)
